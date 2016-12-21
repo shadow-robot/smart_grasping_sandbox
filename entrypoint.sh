b604f23d7553127a2543cc98f436760db27164ae
Ugo Cupcic <ugo@shadowrobot.com>
2016-12-21 15:18:24 +0100

    Finally spawning in gzweb

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
 
-# setup ros environment
+nohup Xvfb :1 -screen 0 1024x768x16 &> xvfb.log &
+DISPLAY=:1.0
+export DISPLAY
+
 source "/workspace/devel/setup.bash"
-source "/usr/share/gazebo-7/setup.sh"
+source "/usr/share/gazebo/setup.sh"
 
-roslaunch smart_grasping_sandbox smart_grasping_sandbox.launch gui:=false &
+roslaunch smart_grasping_sandbox smart_grasping_sandbox.launch gui:=false gzweb:=true verbose:=true &
 
-sleep 60
+sleep 5
 
 cd ~/gzweb
-GAZEBO_MODEL_PATH=/workspace/src:/workspace/src/universal_robot:${GAZEBO_MODEL_PATH} ./start_gzweb.sh
+GAZEBO_MODEL_PATH=/workspace/src:/workspace/src/universal_robot:~/.gazebo/models:${GAZEBO_MODEL_PATH} ./start_gzweb.sh
 
 # wait for any keu
 read -n 1 -s
