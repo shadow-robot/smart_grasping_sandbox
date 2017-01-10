88c1797c9e2ea508f1f4988995efe10859dff841
Ugo Cupcic <ugo@shadowrobot.com>
2017-01-10 21:16:54 +0100

    cleaning up the code a bit

--- smart_grasping_sandbox/scripts/openrave/openrave_grasping.py
+++ smart_grasping_sandbox/scripts/openrave/openrave_grasping.py
@@ -1,42 +1,63 @@
 import openravepy
 import multiprocessing
 import numpy
-
-env = openravepy.Environment()
-env.SetViewer('qtcoin')
-plugin = openravepy.RaveCreateModule(env, "urdf")
-
-name = plugin.SendCommand("load /code/workspace/src/smart_grasping_sandbox/fh_desc/hand_h.urdf /code/workspace/src/smart_grasping_sandbox/fh_desc/hand_h.srdf")
-robot = env.GetRobot(name)
-
-#example getting the transform of the end effector
-#this is a matrix containing orientation and position
-eff = robot.GetManipulators()[0]
-eff.SetChuckingDirection((1,1,1,1,1,1))
-
-target = openravepy.RaveCreateKinBody(env, '')
-target.SetName('cricket_ball')
-target.InitFromSpheres(numpy.array([[0,0,0,0.0375]]),True)
-# also look at target.InitFromTrimesh
-
-#target = env.ReadKinBodyXMLFile('/usr/share/openrave-0.9/data/mug2.kinbody.xml')
-env.Add(target)
-gmodel = openravepy.databases.grasping.GraspingModel(robot,target)
-#gmodel.numthreads = multiprocessing.cpu_count()
-if not gmodel.load():
-  gmodel.autogenerate()
-  gmodel.save()
+import time
 
 
-grasp = gmodel.grasps[0]
-contacts,finalconfig,mindist,volume = gmodel.testGrasp(grasp = grasp, Ngraspingtries=1000,translate=True,forceclosure=True) #graspingnoise
+class GraspEvaluator(object):
+  def __init__(self, urdf_path, srdf_path, chucking_direction, viewer=True):
+    self.__env = openravepy.Environment()
+    plugin = openravepy.RaveCreateModule(self.__env, "urdf")
 
-print "grasp quality = ", mindist
+    if viewer:
+      self.__env.SetViewer('qtcoin')
 
-# Setting grasp from joint values:
-jointvalues = numpy.array([-0.05, 0.4, -0.05, 0.4, -0.05, 0.4, 0])
-robot.SetDOFValues(jointvalues)
+    self.__robot_name = plugin.SendCommand("LoadURI "+urdf_path+" "+srdf_path)
+    self.__robot = self.__env.GetRobot(self.__robot_name)
+
+    self.__end_effector = self.__robot.GetManipulators()[0]
+
+    self.__end_effector.SetChuckingDirection(chucking_direction)
+
+  def evaluate(self, grasp, target):
+    self.__create_target(target)
+
+    grasp_joint_values = numpy.array(grasp)
+    self.__robot.SetDOFValues(grasp_joint_values)
+
+    #contacts,finalconfig,mindist,volume = grasp_model.testGrasp(grasp = grasp,
+    #                                                            Ngraspingtries=1000,
+    #                                                            translate=True,
+    #                                                            forceclosure=True) #graspingnoise
 
 
-gmodel.showgrasp(grasp)
+    #gmodel.showgrasp(grasp)
 
+  def generate_all_grasps(self, target):
+    self.__env.Add(target)
+    gmodel = openravepy.databases.grasping.GraspingModel(self.__robot, target)
+    #gmodel.numthreads = multiprocessing.cpu_count()
+    if not gmodel.load():
+      gmodel.autogenerate()
+      gmodel.save()
+
+  def __create_target(self, target):
+    #TODO Look at target.InitFromTrimesh and use target_path instead of target
+    openrave_target = openravepy.RaveCreateKinBody(self.__env, '')
+    openrave_target.SetName(target)
+    openrave_target.InitFromSpheres(numpy.array([[0, 0, 0, 0.0375]]), True)
+    # target = env.ReadKinBodyXMLFile('/usr/share/openrave-0.9/data/mug2.kinbody.xml')
+
+    self.__env.Add(openrave_target)
+
+if __name__=="__main__":
+  urdf_path = "/code/workspace/src/smart_grasping_sandbox/fh_desc/hand_h.urdf"
+  srdf_path = "/code/workspace/src/smart_grasping_sandbox/fh_desc/hand_h.srdf"
+  chucking_direction = (1,1,1,1,1,1)
+
+  grasp_evaluator = GraspEvaluator(urdf_path, srdf_path, chucking_direction)
+
+  target = 'cricket_ball'
+  grasp = [-0.05, 0.4, -0.05, 0.4, -0.05, 0.4, 0]
+  grasp_evaluator.evaluate(grasp, target)
+  time.sleep(10.)
