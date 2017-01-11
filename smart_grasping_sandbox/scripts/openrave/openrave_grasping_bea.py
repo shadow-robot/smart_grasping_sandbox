b2025ef3588a98a8ae09636d17b3b5a6860a575b
Beatriz Leon <beatriz@shadowrobot.com>
2017-01-11 15:20:15 -0500

    added file to show functions

--- smart_grasping_sandbox/scripts/openrave/openrave_grasping_bea.py
+++ smart_grasping_sandbox/scripts/openrave/openrave_grasping_bea.py
@@ -23,39 +23,24 @@ class GraspEvaluator(object):
 
   def evaluate(self, DOFValues, transform, target):
     self.__create_target(target)
-
-    grasp_joint_values = numpy.array(DOFValues)
-    self.__robot.SetDOFValues(grasp_joint_values)
-    self.__robot.SetTransform(transform)
-    self.__target.SetVisible(True)
+    self.gmodel = openravepy.databases.grasping.GraspingModel(self.__robot, self.__target)
+    self.gmodel.init(friction=0.3, avoidlinks=None)
     
     raw_input('Before testing grasp')
-    DOFValues = [1.49100003, 1.49000003, 1.44000002, -0.01745329]
-    transform = numpy.array([[-0.15986105, 0.96666667, 0.2, -0.02648], [-0.23333333,  0.15986105, -0.9591663,   0.12699362],  [-0.9591663,  -0.2,         0.2,        -0.02648],[ 0.0, 0.0, 0.0, 1.0]])
     grasp_joint_values = numpy.array(DOFValues)
     self.__robot.SetDOFValues(grasp_joint_values)
     self.__robot.SetTransform(transform)
-    self.__target.SetVisible(True)
     grasp = numpy.zeros(self.gmodel.totaldof)
     grasp[self.gmodel.graspindices.get('igrasppreshape')] = grasp_joint_values
-    #grasp[self.gmodel.graspindices.get('igrasptranslationoffset')] = 0.0
+
     contacts,finalconfig,mindist,volume = self.runGraspFromTrans(grasp)
     contactgraph = self.gmodel.drawContacts(contacts) if len(contacts) > 0 else None
     print finalconfig
-    print "mindist", mindist
-    print "volume", volume
-    raw_input('after runGraspFromTrans press any key to continue: ')
-    
-
-
-    #contacts,finalconfig,mindist,volume = grasp_model.testGrasp(grasp = grasp,
-    #                                                            Ngraspingtries=1000,
-    #                                                            translate=True,
-    #                                                            forceclosure=True) #graspingnoise
-    raw_input('press any key to continue: ')
+    print "Mindist: ", mindist
+    print "Volume:", volume
+    raw_input('After testing grasp')
 
   def runGraspFromTrans(self,grasp, finestep=None):
-    """squeeze the fingers to test whether the completed grasp only collides with the target, throws an exception if it fails. Otherwise returns the Grasp parameters. Uses the grasp transformation directly."""
     with self.__robot:
         self.__robot.SetDOFValues(grasp[self.gmodel.graspindices.get('igrasppreshape')],self.gmodel.manip.GetGripperIndices())
         Tmanip = self.gmodel.manip.GetTransform()
@@ -64,10 +49,8 @@ class GraspEvaluator(object):
         self.__robot.SetActiveDOFs(self.gmodel.manip.GetGripperIndices())
         if len(self.gmodel.manip.GetGripperIndices()) == 0:
             return [],[[],self.__robot.GetTransform()],None,None
-        
         finestep = None
         chuckingdirection = self.chucking_direction
-        
         return self.gmodel.grasper.Grasp(transformrobot=False,target=self.gmodel.target,onlycontacttarget=True, forceclosure=True, execute=False, outputfinal=True,translationstepmult=None, finestep=finestep, chuckingdirection=chuckingdirection)
 
   def generate_all_grasps(self, target):
@@ -82,24 +65,6 @@ class GraspEvaluator(object):
     validgrasps,validindices = self.gmodel.computeValidGrasps(checkik=False, returnnum=1)
     validgrasp=validgrasps[0] # choose first grasp
 
-    raw_input('Before testing grasp')
-    DOFValues = [1.49100003, 1.49000003, 1.44000002, -0.01745329]
-    transform = numpy.array([[-0.15986105, 0.96666667, 0.2, -0.02648], [-0.23333333,  0.15986105, -0.9591663,   0.12699362],  [-0.9591663,  -0.2,         0.2,        -0.02648],[ 0.0, 0.0, 0.0, 1.0]])
-    grasp_joint_values = numpy.array(DOFValues)
-    self.__robot.SetDOFValues(grasp_joint_values)
-    self.__robot.SetTransform(transform)
-    self.__target.SetVisible(True)
-    grasp = numpy.zeros(self.gmodel.totaldof)
-    grasp[self.gmodel.graspindices.get('igrasppreshape')] = grasp_joint_values
-    #grasp[self.gmodel.graspindices.get('igrasptranslationoffset')] = 0.0
-    contacts,finalconfig,mindist,volume = self.runGraspFromTrans(grasp)
-    contactgraph = self.gmodel.drawContacts(contacts) if len(contacts) > 0 else None
-    print finalconfig
-    print "mindist", mindist
-    print "volume", volume
-    raw_input('after runGraspFromTrans press any key to continue: ')
-
-    
     forceclosure = True
     graspingnoise = 0.0
     delay = None
@@ -135,18 +100,16 @@ class GraspEvaluator(object):
 
 
 if __name__=="__main__":
-  urdf_path = "~/projects/grasping/src/smart_grasping_sandbox/fh_desc/hand_h.urdf"
-  srdf_path = "~/projects/grasping/src/smart_grasping_sandbox/fh_desc/hand_h.srdf"
-  chucking_direction = (1,1,1,1)
+  urdf_path = "/code/workspace/src/smart_grasping_sandbox/fh_desc/hand_h.urdf"
+  srdf_path = "/code/workspace/src/smart_grasping_sandbox/fh_desc/hand_h.srdf"
+  chucking_direction = (1,1,1,1,1,1)
 
   grasp_evaluator = GraspEvaluator(urdf_path, srdf_path, chucking_direction)
 
-#   target = '/home/ugo/Downloads/hammer.stl'
-  target = 'cricket_ball_1'
-  grasp_evaluator.generate_all_grasps(target)
+  target = 'cricket_ball'
+  #grasp_evaluator.generate_all_grasps(target)
 # 
   DOFValues = [1.49100003, 1.49000003, 1.44000002, -0.01745329]
   transform = numpy.array([[-0.15986105, 0.96666667, 0.2, -0.02648], [-0.23333333,  0.15986105, -0.9591663,   0.12699362],  [-0.9591663,  -0.2,         0.2,        -0.02648],[ 0.0, 0.0, 0.0, 1.0]])
-  print transform
+
   grasp_evaluator.evaluate(DOFValues, transform, target)
-#   time.sleep(10.)
