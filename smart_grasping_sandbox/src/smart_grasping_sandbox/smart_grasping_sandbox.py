e4d2eaf4d7b2945c2fcdffed94aebf7f7837bd54
Ugo Cupcic <ugo@shadowrobot.com>
2017-02-08 10:37:21 +0000

    creating an empty notebook and lib

---
+++ smart_grasping_sandbox/src/smart_grasping_sandbox/smart_grasping_sandbox.py
@@ -0,0 +1,22 @@
+import rospy
+from std_srvs.srv import Empty
+from gazebo_msgs.srv import GetModelState
+from moveit_commander import MoveGroupCommander
+from tf.transformations import quaternion_from_euler
+from math import pi
+from copy import deepcopy
+
+Class SmartGraspingSandbox(object):
+
+    def __init__(self):
+        self.__reset_world = rospy.ServiceProxy("/gazebo/reset_world", Empty)
+        self.__get_ball_pose = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
+        self.__visualisation = rospy.Publisher("~display", Marker, queue_size=1, latch=True)
+        self.__arm_commander = MoveGroupCommander("arm")
+        self.__hand_commander = MoveGroupCommander("hand")
+
+    def reset_world(self):
+        self.__reset_world.call()
+
+    def get_ball_pose(self):
+        return self.__get_ball_pose.call("cricket_ball", "world")
