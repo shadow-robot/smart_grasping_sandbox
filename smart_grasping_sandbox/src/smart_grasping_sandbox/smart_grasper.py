54bda273e7244e80d9ff98b1173d0f4a909a0084
Ugo Cupcic <ugo@shadowrobot.com>
2017-02-28 18:34:19 +0000

    Adding more functionalities to the library (#5)
    * adding get tip pose and move tip to the library
    * adding open / close hand and absolute arm movement

--- smart_grasping_sandbox/src/smart_grasping_sandbox/smart_grasper.py
+++ smart_grasping_sandbox/src/smart_grasping_sandbox/smart_grasper.py
@@ -5,6 +5,8 @@ from moveit_commander import MoveGroupCommander
 from tf.transformations import quaternion_from_euler
 from math import pi
 from copy import deepcopy
+from tf_conversions import posemath, toMsg
+import PyKDL
 
 class SmartGrasper(object):
 
@@ -44,6 +46,73 @@ class SmartGrasper(object):
 
         self.__reset_world.call()
 
+    def get_tip_pose(self):
+        """
+        Gets the current pose of the robot's tooltip in the world frame.
+        @return the tip pose
+        """
+        return self.arm_commander.get_current_pose(self.arm_commander.get_end_effector_link()).pose
+
+    def move_tip_absolute(self, target):
+        """
+        Moves the tooltip to the absolute target in the world frame
+
+        @param target is a geometry_msgs.msg.Pose
+        @return True on success
+        """
+        self.arm_commander.set_start_state_to_current_state()
+        self.arm_commander.set_pose_targets([target])
+        plan = self.arm_commander.plan()
+        if not self.arm_commander.execute(plan):
+            return False
+        return True
+        
+
+    def move_tip(self, x=0., y=0., z=0., roll=0., pitch=0., yaw=0.):
+        """
+        Moves the tooltip in the world frame by the given x,y,z / roll,pitch,yaw. 
+
+        @return True on success
+        """
+        transform = PyKDL.Frame(PyKDL.Rotation.RPY(pitch, roll, yaw),
+                                PyKDL.Vector(-x, -y, -z))
+        
+        tip_pose = self.get_tip_pose()
+        tip_pose_kdl = posemath.fromMsg(tip_pose)
+        final_pose = toMsg(tip_pose_kdl * transform)
+            
+        self.arm_commander.set_start_state_to_current_state()
+        self.arm_commander.set_pose_targets([final_pose])
+        plan = self.arm_commander.plan()
+        if not  self.arm_commander.execute(plan):
+            return False
+        return True
+
+
+    def open_hand(self):
+        """
+        Opens the hand.
+        @return True on success
+        """
+        self.hand_commander.set_named_target("open")
+        plan = self.hand_commander.plan()
+        if not self.hand_commander.execute(plan, wait=True):
+            return False
+
+        return True
+
+    def close_hand(self):
+        """
+        Closes the hand.
+        @return True on success
+        """
+        self.hand_commander.set_named_target("close")
+        plan = self.hand_commander.plan()
+        if not self.hand_commander.execute(plan, wait=True):
+            return False
+
+        return True
+            
     def pick(self):
         """
         Does its best to pick the ball.
